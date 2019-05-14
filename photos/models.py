fbeacc4707f86dc47d7bebb145d90122ce725aff
kennymaina <victormurigi@outlook.com>
2019-05-14 15:47:16 +0300

    creating class location in models.py

--- photos/models.py
+++ photos/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 
 # Create your models here.
-# class Category(models.Model):
-#     name=models.CharField(max_length=30)
-
-#     def __str__(self):
-#         return self.name
-#     class Meta:
-#         verbose_name_plural = "Categories"
-
-
-#     def save_category(self):
-#         self.save()
-
-#     def delete_category(self):
-#         self.delete()
-
-class Location(models.Model):
+class Category(models.Model):
     name=models.CharField(max_length=30)
-
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
 
-    def save_location(self):
+
+    def save_category(self):
         self.save()
 
-    def delete_location(self):
+    def delete_category(self):
         self.delete()
+
+# class Location(models.Model):
+#     name=models.CharField(max_length=30)
+
+
+#     def __str__(self):
+#         return self.name
+
+#     def save_location(self):
+#         self.save()
+
+#     def delete_location(self):
+#         self.delete()
 
 class Image(models.Model):
     image=models.ImageField(upload_to ='images/')
