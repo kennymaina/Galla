652d58769506226736f032e95b51fe25f8562e79
kennymaina <victormurigi@outlook.com>
2019-05-14 15:10:39 +0300

    image class added in models.py with specified image logs

--- photos/models.py
+++ photos/models.py
@@ -29,12 +29,12 @@ class Location(models.Model):
     def delete_location(self):
         self.delete()
 
-class Image(models.Model):
-    image=models.ImageField(upload_to ='images/')
-    name=models.CharField(max_length =30)
-    description=models.TextField()
-    category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    location=models.ForeignKey(Location,on_delete=models.CASCADE)
+# class Image(models.Model):
+#     image=models.ImageField(upload_to ='images/')
+#     name=models.CharField(max_length =30)
+#     description=models.TextField()
+#     category=models.ForeignKey(Category,on_delete=models.CASCADE)
+#     location=models.ForeignKey(Location,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -66,7 +66,7 @@ class Image(models.Model):
         photos=cls.objects.filter(id=location_id)
         return photos
 
-    # @classmethod
-    # def filter_by_category(cls,category_id):
-    #     photos=cls.objects.filter(id=category_id)
-    #     return photos
+    @classmethod
+    def filter_by_category(cls,category_id):
+        photos=cls.objects.filter(id=category_id)
+        return photos
