f8fbe1f0eeb22b44ce695f83813ab4d908019855
kennymaina <victormurigi@outlook.com>
2019-05-14 15:53:55 +0300

    image details

--- photos/views.py
+++ photos/views.py
@@ -48,17 +48,17 @@ def location(request,location_id):
     return render(request,'location.html',{"photos":photos})
 
 
-# def category(request,category_id):
-#     photos=Image.objects.filter(category_id=category_id)
+def category(request,category_id):
+    photos=Image.objects.filter(category_id=category_id)
 
-#     return render(request,'category.html',{"photos":photos})
+    return render(request,'category.html',{"photos":photos})
 
-def imagedetails(request,image_id):
-    try:
-        image = Image.objects.get(id=image_id)
-    except DoesNotExist:
-         raise Http404()
-    return render(request,"image.html",{"image":image})
+# def imagedetails(request,image_id):
+#     try:
+#         image = Image.objects.get(id=image_id)
+#     except DoesNotExist:
+#          raise Http404()
+#     return render(request,"image.html",{"image":image})
 
 def copy_image(from_model, to_model):
     to_model.image.save(from_model.image.url.split('/')[-1],from_model.image.file,save=True)
